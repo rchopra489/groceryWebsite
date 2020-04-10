bca1df7739e1286cae1611c79e1e63c31fd21d72
rchopra489 <rchopra489@gmail.com>
2020-04-11 04:49:28 +0530

    delete file

--- grossary.js
+++
@@ -1,9 +0,0 @@
-function getData(){
-    firebase.database().ref('/').once('value', function(snapshot){
-        snapshot.forEach(function(childSnapshot){
-            var childKey = childSnapshot.key;
-            var childData = childSnapshot.val();
-            document.getElementsByClassName("products").children[0] = childData.image;
-        })
-    })
-}
